c774274426ee2c42ba70bf824a0a34cb4cc82062
Akinuliola Akinbobola <akinuliola_a@yahoo.com>
2022-05-21 17:28:40 +0100

    update

--- js/index.js
+++ js/index.js
@@ -19,3 +19,20 @@ function onSubmit(e) {
     message.innerHTML = "Successfull";
   }
 }
+
+//HAMBUGER MENU
+const mainMenu = document.querySelector(".mainmenu");
+const closeMenu = document.querySelector(".closemenu");
+const openMenu = document.querySelector(".openmenu");
+
+openMenu.addEventListener("click", show);
+closeMenu.addEventListener("click", close);
+
+function show() {
+  mainMenu.style.display = "flex";
+  mainMenu.style.top = "0";
+}
+
+function close() {
+  mainMenu.style.top = "-100%";
+}
